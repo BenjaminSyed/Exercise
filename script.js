ae0603cf2fd83c4db63177e4b80b7b1fd65a2a30
BenjaminSyed02100 <135163695+BenjaminSyed@users.noreply.github.com>
2023-08-09 11:30:20 +0200

    Just a Website

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-function Task2() {
-  var formOfAddress = document.getElementById("formOfAddress").value;
-  var firstName = document.getElementById("firstName").value;
-  var lastName = document.getElementById("lastName").value;
-  var greeting = "";
-
-// Bei Auswahl der jeweiligen Anrede, kommt die ensprechende Bezeichnung dazu (Dead Mr/ Dear Madam)
-
-  if (formOfAddress === "Mr.") {
-    greeting = "Dear Mr. " + lastName;
-  } else if (formOfAddress === "Mrs.") {
-    greeting = "Dear Madam " + lastName;
-  }
-
-  // Diese Zeile dient um das Ergebnis darzustellen.
-
-  document.getElementById("greeting").textContent = greeting;
-  
-}
+var i = 0;
+    var txt = 'Willkommen auf meiner Website! Mein Name ist Benni. Lesen Sie hier etwas über mich, meine Vergangenheit und meine Zukunft.'; 
+    var speed = 40; 
+    
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
